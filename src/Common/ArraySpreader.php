51fa9a687f41426a8645e8e2e3fc8bd0d0bbdfea
TiMESPLiNTER <github@timesplinter.ch>
2017-07-20 12:45:43 +0200

    Optimized spread operator handling in arrays

--- src/Common/ArraySpreader.php
+++ src/Common/ArraySpreader.php
@@ -20,30 +20,10 @@ class ArraySpreader extends NodeVisitorAbstract
 	/** @var array */
 	private $unpacks = [];
 
-	/** @var int */
-	private $pos = 0;
-
-	/** @var array */
-	private $arrayCounts = [];
-
 	public function enterNode(Node $node)
 	{
-		// Take all further ArrayItems out of the array,
-		// Append new statement for array += spreaded variable,
-		// Append new array node with rest of ArrayItems
         if (true === $node instanceof Node\Expr\Array_) {
             $this->currentArrayNode = $node;
-
-            // Keep track of array elements
-            $var = $node->getAttribute(NodeConnector::ATTR_PREV);
-            $this->arrayCounts[$var->name] = count($node->items);
-        } elseif (true === $node instanceof UnpackArrayItem) {
-            $this->unpacks[] = [
-                'node' => $node,
-                'pos' => $this->pos
-            ];
-        } elseif (true === $node instanceof Node\Expr\ArrayItem) {
-            ++$this->pos;
         }
 
 		return null;
@@ -51,38 +31,41 @@ class ArraySpreader extends NodeVisitorAbstract
 
 	public function leaveNode(Node $node) {
 	    if ($node instanceof UnpackArrayItem) {
-	        return NodeTraverser::REMOVE_NODE;
+	        $id = '__spread_'.uniqid();
+            $this->unpacks[$id] = $node;
+
+            return new Node\Expr\ArrayItem(new Node\Scalar\String_($id));
         } elseif ($this->currentArrayNode === null || $node !== $this->currentArrayNode->getAttribute(NodeConnector::ATTR_PARENT)) {
 	        return null;
         }
 
         $sliceNodes = [$node];
-	    $offset = 0;
 
-        foreach ($this->unpacks as $unpack) {
-            //echo 'do an unpack of ' , $unpack['node']->value->name , ' (count: ',$this->arrayCounts[$unpack['node']->value->name],') at pos ' ,  $unpack['pos'] , PHP_EOL;
+        foreach ($this->unpacks as $id => $unpack) {
             /** @var Node $parent */
 			$prev = $this->currentArrayNode->getAttribute(NodeConnector::ATTR_PREV);
 
-			$unpackNode = $unpack['node']->value;
-			$unpackPos = $unpack['pos'];
+			$unpackNode = $unpack->value;
 
             $sliceNodes[] = new Node\Expr\FuncCall(
                 new Node\Name('array_splice'),
                 [
                     $prev,
-                    new Node\Scalar\LNumber($unpackPos+$offset),
-                    new Node\Scalar\LNumber(0),
+                    new Node\Expr\FuncCall(
+                        new Node\Name('array_search'),
+                        [
+                            new Node\Scalar\String_($id),
+                            $prev,
+                        ]
+                    ),
+                    new Node\Scalar\LNumber(1),
                     $unpackNode
                 ]
             );
-
-            $offset += $this->arrayCounts[$unpack['node']->value->name];
         }
 
         $this->currentArrayNode = null;
         $this->unpacks = [];
-        $this->pos = 0;
 
         return $sliceNodes;
 	}
