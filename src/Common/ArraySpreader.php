50629246f8930685a7ff03f9064482567127fbc6
TiMESPLiNTER <github@timesplinter.ch>
2017-07-20 15:56:05 +0200

    Wip

--- src/Common/ArraySpreader.php
+++ src/Common/ArraySpreader.php
@@ -14,7 +14,10 @@ use Phx\Parser\Node\Expr\UnpackArrayItem;
  */
 class ArraySpreader extends NodeVisitorAbstract
 {
-    /** @var null|Node\Expr\Array_ */
+    /** @var NodeRef[] */
+	private $currentArrayNodes = [];
+
+	/** @var null|NodeRef */
 	private $currentArrayNode = null;
 
 	/** @var array */
@@ -23,31 +26,44 @@ class ArraySpreader extends NodeVisitorAbstract
 	public function enterNode(Node $node)
 	{
         if (true === $node instanceof Node\Expr\Array_) {
-            $this->currentArrayNode = $node;
+            //var_dump('uhh oh I found an array: ' . count($node->items));
+            $this->currentArrayNode = $this->currentArrayNodes[] = new NodeRef($node);
+            //echo 'array stack: ' , count($this->currentArrayNodes) , PHP_EOL;
         }
 
 		return null;
 	}
 
 	public function leaveNode(Node $node) {
+	    $dumper = new NodeDumper();
 	    if ($node instanceof UnpackArrayItem) {
 	        $id = '__spread_'.uniqid();
-            $this->unpacks[$id] = $node;
+            $this->currentArrayNode->unpacks[$id] = $node;
+
+            echo 'spread: ',$id,PHP_EOL;
+
+            //var_dump($id, $dumper->dump($node)); echo '------------------------>',PHP_EOL;
 
             return new Node\Expr\ArrayItem(new Node\Scalar\String_($id));
-        } elseif ($this->currentArrayNode === null || $node !== $this->currentArrayNode->getAttribute(NodeConnector::ATTR_PARENT)) {
-	        return null;
+        } elseif ($this->currentArrayNode === null || $node !== $this->currentArrayNode->node->getAttribute(NodeConnector::ATTR_PARENT)) {
+	        if ($this->currentArrayNode !== null) {
+                echo 'skip that: ', get_class($this->currentArrayNode->node->getAttribute(NodeConnector::ATTR_PARENT)), PHP_EOL;
+            }
+            return null;
         }
-
-        $sliceNodes = [$node];
-
-        foreach ($this->unpacks as $id => $unpack) {
+        echo $dumper->dump($node);
+        $spliceNodes = [$node];
+foreach ($this->currentArrayNode->unpacks as $id => $unpackNode) {
+    echo PHP_EOL,'----------',PHP_EOL; var_dump($id, $dumper->dump($unpackNode));
+}
+        echo PHP_EOL,'------------------------>',PHP_EOL;
+        foreach ($this->currentArrayNode->unpacks as $id => $unpack) {
             /** @var Node $parent */
-			$prev = $this->currentArrayNode->getAttribute(NodeConnector::ATTR_PREV);
+			$prev = $this->currentArrayNode->node->getAttribute(NodeConnector::ATTR_PREV);
 
 			$unpackNode = $unpack->value;
 
-            $sliceNodes[] = new Node\Expr\FuncCall(
+            $spliceNodes[] = new Node\Expr\FuncCall(
                 new Node\Name('array_splice'),
                 [
                     $prev,
@@ -64,9 +80,30 @@ class ArraySpreader extends NodeVisitorAbstract
             );
         }
 
-        $this->currentArrayNode = null;
-        $this->unpacks = [];
+        array_pop($this->currentArrayNodes);
 
-        return $sliceNodes;
+        if (false === $this->currentArrayNode = end($this->currentArrayNodes)) {
+            $this->currentArrayNode = null;
+        }
+
+        return $spliceNodes;
 	}
+}
+
+class NodeRef
+{
+    /** @var Node\Expr\Array_ */
+    public $node = null;
+
+    /** @var array UnpackArrayItem[] */
+    public $unpacks = [];
+
+    /**
+     * NodeRef constructor.
+     * @param null $node
+     */
+    public function __construct($node)
+    {
+        $this->node = $node;
+    }
 }
