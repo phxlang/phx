e96b92c0fd67d68db1ea7b6aaf0e2bc1662282bd
TiMESPLiNTER <github@timesplinter.ch>
2017-07-20 21:53:48 +0200

    Add ForIn extension

---
+++ src/Extension/ForIn/ForInExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Phx\Extension\ForIn;
+use PhpParser\Node\Stmt\Foreach_;
+use Phx\Extension\TokenExtension;
+use Phx\Extension\YaccExtension;
+
+/**
+ * @author Pascal Muenst <pascal@timesplinter.ch>
+ */
+class ForInExtension implements TokenExtension, YaccExtension
+{
+
+    /**
+     * @return array
+     */
+    public function extendTokens(): array
+    {
+        return ['%token T_IN'];
+    }
+
+    /**
+     * @return array
+     */
+    public function extendYacc(): array
+    {
+        return [
+            'non_empty_statement' => [
+                "T_FOR '(' foreach_variable T_IN expr ')' foreach_statement" =>
+                    "{ $$ = ".Foreach_::class."[$5, $3[0], ['keyVar' => null, 'byRef' => $3[1], 'stmts' => $7]]; }"
+            ]
+        ];
+    }
+}
